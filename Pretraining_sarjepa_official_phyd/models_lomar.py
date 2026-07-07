bceffbfc93f5b00757a9632149944c1a0598f9ca
zhangji <1320779661@qq.com>
2026-07-07 12:10:19 +0800

    Support multichannel PhyD targets in SAR-JEPA patchify

--- Pretraining_sarjepa_official_phyd/models_lomar.py
+++ Pretraining_sarjepa_official_phyd/models_lomar.py
@@ -403,30 +403,31 @@ class MaskedAutoencoderViT(nn.Module):
 
     def patchify(self, imgs):
         """
-        imgs: (N, 3, H, W)
-        x: (N, L, patch_size**2 *3)
+        imgs: (N, C, H, W)
+        x: (N, L, patch_size**2 * C)
         """
         p = self.patch_embed.patch_size[0]
         assert imgs.shape[2] == imgs.shape[3] and imgs.shape[2] % p == 0
 
         h = w = imgs.shape[2] // p
-        x = imgs.reshape(shape=(imgs.shape[0], 1, h, p, w, p))
+        c = imgs.shape[1]
+        x = imgs.reshape(shape=(imgs.shape[0], c, h, p, w, p))
         x = torch.einsum('nchpwq->nhwpqc', x)
-        x = x.reshape(shape=(imgs.shape[0], h * w, p**2 * 1))
+        x = x.reshape(shape=(imgs.shape[0], h * w, p**2 * c))
         return x
 
-    def unpatchify(self, x):
+    def unpatchify(self, x, channels=1):
         """
-        x: (N, L, patch_size**2 *3)
-        imgs: (N, 3, H, W)
+        x: (N, L, patch_size**2 * C)
+        imgs: (N, C, H, W)
         """
         p = self.patch_embed.patch_size[0]
         h = w = int(x.shape[1]**.5)
         assert h * w == x.shape[1]
         
-        x = x.reshape(shape=(x.shape[0], h, w, p, p, 1))
+        x = x.reshape(shape=(x.shape[0], h, w, p, p, channels))
         x = torch.einsum('nhwpqc->nchpwq', x)
-        imgs = x.reshape(shape=(x.shape[0], 1, h * p, h * p))
+        imgs = x.reshape(shape=(x.shape[0], channels, h * p, h * p))
         return imgs
 
     def random_masking(self, x, mask_ratio):
